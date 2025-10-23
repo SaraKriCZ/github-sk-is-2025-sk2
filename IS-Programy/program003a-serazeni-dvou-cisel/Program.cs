d10c79e27e0be1590004d8fddf7d95210a5ab25c
Sára Kristanová <sara.kristanova@tul.cz>
2025-10-23 13:13:04 +0200

    #6 program seřazení dvou čísel

---
+++ IS-Programy/program003a-serazeni-dvou-cisel/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+string again = "a";
+        while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("********************************************");
+            Console.WriteLine("*********** Výpis číselné řady *************");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("************* Sára Kristanová ******************");
+            Console.WriteLine("************** 23.10.2025 *******************");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("********************************************");
+            Console.WriteLine();
+
+
+          
+            Console.Write("Zadejte celé číslo A: ");
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.Write("Nezadali jste celé číslo. Zadejte celé číslo A znovu: ");
+            }
+                       
+            Console.Write("Zadejte celé číslo B: ");
+            int b;
+            while (!int.TryParse(Console.ReadLine(), out b))
+            {
+                Console.Write("Nezadali jste celé číslo. Zadejte celé číslo B znovu: ");
+            }
+
+    Console.WriteLine();
+
+    int pom;
+    //Chceme seřadit čísla  vzestupně     
+    if (a > b)
+    {
+        pom = a;
+        a = b;
+        b = pom;
+        Console.WriteLine("Došlo k prohození proměnných")
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("=================================");
+    Console.WriteLine($"Seřazená čísla: {a}, {b}");
+    Console.WriteLine("=================================");
+ 
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Pro opakování programu stiskněte klávesu 'a', nebo jinou pro ukončení: ");
+            again = Console.ReadLine()!;
+        }
