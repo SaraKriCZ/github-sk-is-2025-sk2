cf333d33f2299d2dea87ad431db5a72353846359
Sára Kristanová <sara.kristanova@tul.cz>
2025-10-29 22:02:07 +0100

    #7 program vykreslení noční oblohy

---
+++ IS-Programy/program004e-nocni-obloha/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+        string again = "a";
+        while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("********************************************");
+            Console.WriteLine("********* Vykreslení noční oblohy***********");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("************ Sára Kristanová ***************");
+            Console.WriteLine("************** 29.10.2025 *******************");
+            Console.WriteLine("********************************************");
+            Console.WriteLine("********************************************");
+            Console.WriteLine();
+
+            Console.Write("Zadejte šířku oblohy: ");
+            int width;
+            while (!int.TryParse(Console.ReadLine(), out width))
+            {
+                Console.Write("Nezadali jste celé číslo. Zadejte šířku oblohy: ");
+            }
+            
+            Console.Write("Zadejte výšku oblohy: ");
+            int height;
+            while (!int.TryParse(Console.ReadLine(), out height))
+            {
+                Console.Write("Nezadali jste celé číslo. Zadejte výšku oblohy: ");
+            }
+            
+            for (int i = 0; i < height; i++)
+            {
+            for (int j = 0; j < width; j++)
+            {
+            if ((i + j) % 2 == 0)
+            Console.Write("*");
+            else
+            Console.Write(" ");
+            }
+            Console.WriteLine();
+            }
+
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Pro opakování programu stiskněte klávesu 'a', nebo jinou pro ukončení: ");
+            again = Console.ReadLine()!;
+        }
