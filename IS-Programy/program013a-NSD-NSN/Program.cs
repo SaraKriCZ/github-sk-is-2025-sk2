2f50862798e42d71d0a0c296abdd4be82846599f
Sára Kristanová <sara.kristanova@tul.cz>
2025-12-11 13:48:44 +0100

    #14 NSD a NSN

---
+++ IS-Programy/program013a-NSD-NSN/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+
+    // Metoda pro razítko - hlavičku
+    razitko();
+
+    // Načítání hodnot
+    ulong a = nactiCislo("Zadejte číslo a: ");
+    ulong b = nactiCislo("Zadejte číslo b: ");
+
+    ulong nsd = vypocitatNSD(a, b);
+    ulong nsn = vypocitatNSN(a, b, nsd);
+
+
+    zobrazitVysledky(a, b, nsd, nsn);
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+}
+
+/* Hlavní program - konec */
+
+/* metoda která nic nevrací - nevrací hodnotu */
+static void razitko()
+{
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Výpočet NSD a NSN *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Sára Kristanová ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+}    
+
+/* metoda pro načtení čísel */
+static ulong nactiCislo(string zprava)
+{
+    Console.Write(zprava);
+    ulong cislo;
+    while (!ulong.TryParse(Console.ReadLine(), out cislo))
+    {
+        Console.Write("Nezadali jste přirozené číslo. Zadejte hodnotu znovu: ");
+    }
+
+    // !!! Metoda, která vrací nějaký konkrétní datový typ, musí obsahovat následující řádek
+    return cislo;
+
+}
+
+//není stejná proměnná
+static ulong vypocitatNSD(ulong a, ulong b)
+{
+    while (a != b)
+    {
+        if(a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
+}
+
+static void zobrazitVysledky(ulong a, ulong b, ulong nsd, ulong nsn)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine();
+    Console.WriteLine("================================");
+    Console.WriteLine($"NSD čísel {a} a {b} je {nsd}");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("================================");
+    Console.WriteLine($"NSN čísel {a} a {b} je {nsn}");
+
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+static ulong vypocitatNSN(ulong a, ulong b, ulong nsd)
+{
+    //vysledekNSN = (a*b)/nsd;
+    //return vysledekNSN;
+    return (a*b)/nsd;
+}
